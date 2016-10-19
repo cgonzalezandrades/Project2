b8ac64618aa7954fd1d9310ea2ddac3ea2b68693
Dad <Dad>
2016-10-19 18:48:45 -0400

    first commit

---
+++ spec/Test/spec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Parents on 10/17/2016.
+ */
+
+//
+// userLogin("","hjkfshlkd"){
+// 	var aBoolean
+// 	///.....
+// 	return aBoolean;
+// }
+//
+// var username = userLogin("banmae","pass123")usernema""
